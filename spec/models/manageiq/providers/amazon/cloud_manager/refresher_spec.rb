ManageIQ/manageiq-providers-vmware

Update references to `Vm#vendor` and `Vm#vendor=`
Setters/getters will need to expect lowercase for these now. If a getter
is expecting mixed case for display reasons, use the new
`#vendor_display` method instead.


(transferred from ManageIQ/manageiq@3c0f5f678d2d61de7b1ef4b498487f466b4b0de7)

spec/models/manageiq/providers/vmware/infra_manager/refresher_spec.rb
@@ -346,7 +346,7 @@ describe ManageIQ::Providers::Vmware::InfraManager::Refresher do
       :ems_ref               => "vm-11342",
       :ems_ref_obj           => VimString.new("vm-11342", :VirtualMachine, :ManagedObjectReference),
       :uid_ems               => "422f5d16-c048-19e6-3212-e588fbebf7e0",
-      :vendor                => "VMware",
+      :vendor                => "vmware",
       :power_state           => "off",
       :location              => "JoeF 4.0.1/JoeF 4.0.1.vmx",
       :tools_status          => "toolsNotRunning",

ManageIQ/manageiq-providers-amazon

spec/models/manageiq/providers/amazon/cloud_manager/refresher_spec.rb
@@ -226,7 +226,7 @@ describe ManageIQ::Providers::Amazon::CloudManager::Refresher do
       :ems_ref               => "ami-5769193e",
       :ems_ref_obj           => nil,
       :uid_ems               => "ami-5769193e",
-      :vendor                => "Amazon",
+      :vendor                => "amazon",
       :power_state           => "never",
       :location              => "200278856672/EmsRefreshSpec-Image",
       :tools_status          => nil,
@@ -285,7 +285,7 @@ describe ManageIQ::Providers::Amazon::CloudManager::Refresher do
       :ems_ref               => "i-680071e9",
       :ems_ref_obj           => nil,
       :uid_ems               => "i-680071e9",
-      :vendor                => "Amazon",
+      :vendor                => "amazon",
       :power_state           => "on",
       :location              => "ec2-54-221-202-53.compute-1.amazonaws.com",
       :tools_status          => nil,
@@ -365,7 +365,7 @@ describe ManageIQ::Providers::Amazon::CloudManager::Refresher do
       :ems_ref               => "i-6eeb97ef",
       :ems_ref_obj           => nil,
       :uid_ems               => "i-6eeb97ef",
-      :vendor                => "Amazon",
+      :vendor                => "amazon",
       :power_state           => "off",
       :location              => "unknown",
       :tools_status          => nil,
@@ -445,7 +445,7 @@ describe ManageIQ::Providers::Amazon::CloudManager::Refresher do
       :ems_ref               => "i-8b5739f2",
       :ems_ref_obj           => nil,
       :uid_ems               => "i-8b5739f2",
-      :vendor                => "Amazon",
+      :vendor                => "amazon",
       :power_state           => "on",
       :location              => "unknown",
       :tools_status          => nil,