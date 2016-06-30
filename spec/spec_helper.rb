959b24f19f4681acc08d974d0e911c709fd21e5d
Marcel Hild <hild@b4mad.net>
2016-06-30 15:38:28 +0200

    move vcr cassettes for amazon from manageiq repo over

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,5 @@ require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 VCR.configure do |config|
   config.ignore_hosts 'codeclimate.com'
+  config.cassette_library_dir = File.join(ManageIQ::Providers::Amazon::Engine.root, 'spec/vcr_cassettes')
 end
