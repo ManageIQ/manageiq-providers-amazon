a1461d61548435f38761de1d4a4b5185e2cd4e82
Jillian Tullo <jtullo@redhat.com>
2016-06-30 10:31:07 -0400

    minor updates to virtual templates

--- app/models/manageiq/providers/amazon/cloud_manager/virtual_template.rb
+++ app/models/manageiq/providers/amazon/cloud_manager/virtual_template.rb
@@ -1,3 +1,8 @@
 class ManageIQ::Providers::Amazon::CloudManager::VirtualTemplate < ::ManageIQ::Providers::CloudManager::VirtualTemplate
-  validates :cloud_subnet_id, :availability_zone_id, :ems_ref, :presence => true
+  validates :cloud_network_id, :availability_zone_id,
+            :ems_ref, :ems_id, :flavor_id, presence: true
+
+  def self.vendor
+    @vendor ||= 'amazon'.freeze
+  end
 end
