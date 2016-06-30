aa8d69fb78b21dcc3f6ba668b5617d92934d7a62
Jillian Tullo <jtullo@redhat.com>
2016-06-30 11:24:56 -0400

    removing vendor

--- app/models/manageiq/providers/amazon/cloud_manager/virtual_template.rb
+++ app/models/manageiq/providers/amazon/cloud_manager/virtual_template.rb
@@ -1,8 +1,4 @@
 class ManageIQ::Providers::Amazon::CloudManager::VirtualTemplate < ::ManageIQ::Providers::CloudManager::VirtualTemplate
   validates :cloud_network_id, :availability_zone_id,
             :ems_ref, :ems_id, :flavor_id, presence: true
-
-  def self.vendor
-    @vendor ||= 'amazon'.freeze
-  end
 end
