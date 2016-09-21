616ecc680196711018a3940381a1dd1b6ca30518
Marcel Hild <hild@b4mad.net>
2016-09-21 12:02:14 +0200

    move floating ip factory from manageiq

---
+++ spec/factories/floating_ip.rb
@@ -0,0 +1,4 @@
+FactoryGirl.define do
+  factory :floating_ip_amazon, :parent => :floating_ip,
+                               :class  => "ManageIQ::Providers::Amazon::NetworkManager::FloatingIp"
+end
