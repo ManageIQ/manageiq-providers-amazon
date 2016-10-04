00e55476a24354ea945ad092cf2bfcbc66ab0042
Ladislav Smola <lsmola@redhat.com>
2016-10-04 13:03:42 +0200

    Don't define the dependecies manually
    Don't define the dependecies manually, we can fetch them
    automatically.

--- app/models/manageiq/providers/amazon/network_manager/cloud_subnet.rb
+++ app/models/manageiq/providers/amazon/network_manager/cloud_subnet.rb
@@ -1,6 +1,5 @@
 class ManageIQ::Providers::Amazon::NetworkManager::CloudSubnet < ::CloudSubnet
   include DtoMixin
 
-  dto_dependencies :cloud_networks
   dto_attributes :type, :ems_ref, :name, :cidr, :status, :availability_zone, :cloud_network
 end
--- app/models/manageiq/providers/amazon/network_manager/security_group.rb
+++ app/models/manageiq/providers/amazon/network_manager/security_group.rb
@@ -1,5 +1,5 @@
 class ManageIQ::Providers::Amazon::NetworkManager::SecurityGroup < ::SecurityGroup
   include DtoMixin
-  dto_dependencies :cloud_network, :orchestration_stack
+
   dto_attributes :type, :ems_ref, :name, :description, :cloud_network, :orchestration_stack
 end
