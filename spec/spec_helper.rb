46b9dcb13428836204fc157972b91d8de73fd02c
Greg Blomquist <blomquisg@gmail.com>
2016-10-13 08:48:20 -0400

    Merge pull request #49 from durandom/floating_ip_shared_example
    Floating ip spec as shared example
    (cherry picked from commit 03bf912b9243bd09218273fd79b70eeb7cfe10ab)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,5 @@ VCR.configure do |config|
   config.ignore_hosts 'codeclimate.com' if ENV['CI']
   config.cassette_library_dir = File.join(ManageIQ::Providers::Amazon::Engine.root, 'spec/vcr_cassettes')
 end
+
+Dir[Rails.root.join("spec/shared/**/*.rb")].each { |f| require f }
