96722e0fc8656847649a5f84a7bea0304a5e68ec
Adam Grare <adam@grare.com>
2016-12-02 12:03:39 -0500

    Include additional_regions from settings
    https://bugzilla.redhat.com/show_bug.cgi?id=1389068

--- spec/models/manageiq/providers/amazon/regions_spec.rb
+++ spec/models/manageiq/providers/amazon/regions_spec.rb
@@ -15,4 +15,64 @@ describe ManageIQ::Providers::Amazon::Regions do
       expect(described_class.names).not_to include('us-gov-west-1')
     end
   end
+
+  context "add regions via settings" do
+    context "with no additional regions set" do
+      let(:settings) do
+        {:ems => {:ems_amazon => {:additional_regions => nil}}}
+      end
+
+      it "returns standard regions" do
+        stub_settings(settings)
+        expect(described_class.names).to eql(described_class::REGIONS.keys)
+      end
+    end
+
+    context "with one additional" do
+      let(:settings) do
+        {
+          :ems => {
+            :ems_amazon => {
+              :additional_regions => {
+                :"my-custom-region" => {
+                  :name => "My First Custom Region"
+                }
+              }
+            }
+          }
+        }
+      end
+
+      it "returns the custom regions" do
+        stub_settings(settings)
+        expect(described_class.names).to include("my-custom-region")
+      end
+    end
+
+    context "with additional regions and disabled regions" do
+      let(:settings) do
+        {
+          :ems => {
+            :ems_amazon => {
+              :disabled_regions   => ["my-custom-region-2"],
+              :additional_regions => {
+                :"my-custom-region-1" => {
+                  :name => "My First Custom Region"
+                },
+                :"my-custom-region-2" => {
+                  :name => "My Second Custom Region"
+                }
+              }
+            }
+          }
+        }
+      end
+
+      it "disabled_regions overrides additional_regions" do
+        stub_settings(settings)
+        expect(described_class.names).to     include("my-custom-region-1")
+        expect(described_class.names).not_to include("my-custom-region-2")
+      end
+    end
+  end
 end
