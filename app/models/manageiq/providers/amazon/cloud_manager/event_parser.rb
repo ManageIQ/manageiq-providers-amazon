2095fdf3b2b36e4c6faac296c14f6949a735bded
Marcel Hild <hild@b4mad.net>
2016-12-16 16:24:54 +0100

    map event unknown to automate to ChangeNotification

--- app/models/manageiq/providers/amazon/cloud_manager/event_parser.rb
+++ app/models/manageiq/providers/amazon/cloud_manager/event_parser.rb
@@ -1,4 +1,31 @@
 module ManageIQ::Providers::Amazon::CloudManager::EventParser
+  # these have their own EmsEvent classes in automate
+  INSTANCE_EVENTS = %w(
+    AWS_EC2_Instance_CREATE
+    AWS_EC2_Instance_UPDATE
+    AWS_EC2_Instance_DELETE
+    AWS_EC2_Instance_running
+    AWS_EC2_Instance_shutting-down
+    AWS_EC2_Instance_stopped
+  ).to_set.freeze
+
+  def self.map_event_type(aws_event_type)
+    if INSTANCE_EVENTS.include?(aws_event_type)
+      aws_event_type
+    else
+      'ConfigurationItemChangeNotification'
+    end
+  end
+
+  def self.format_message(event)
+    changed_properties = event.fetch_path("configurationItemDiff", "changedProperties")
+    if changed_properties.present?
+      "#{event['eventType']}: #{changed_properties}"
+    else
+      "#{event['eventType']}"
+    end
+  end
+
   def self.event_to_hash(event, ems_id)
     log_header = "ems_id: [#{ems_id}] " unless ems_id.nil?
 
@@ -6,9 +33,9 @@ module ManageIQ::Providers::Amazon::CloudManager::EventParser
                "#{event["configurationItem"]["resourceId"]}]")
 
     event_hash = {
-      :event_type => event["eventType"],
+      :event_type => map_event_type(event["eventType"]),
       :source     => "AMAZON",
-      :message    => event["configurationItemDiff"],
+      :message    => format_message(event),
       :timestamp  => event["notificationCreationTime"],
       :full_data  => event,
       :ems_id     => ems_id
