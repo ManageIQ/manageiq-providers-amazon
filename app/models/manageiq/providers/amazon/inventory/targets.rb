f8df524c8973d1c3bc7f84a732cd6224fb93898d
Ladislav Smola <lsmola@redhat.com>
2017-01-06 16:27:35 +0100

    A targets base class
    Contains a helper for adding an InventoryCollection into
    inventory. And a helper for parsing AWS event payload.

---
+++ app/models/manageiq/providers/amazon/inventory/targets.rb
@@ -0,0 +1,32 @@
+class ManageIQ::Providers::Amazon::Inventory::Targets < ManageIQ::Providers::Amazon::Inventory
+  include ManageIQ::Providers::Amazon::Inventory::InventoryCollectionDefaultInitData
+
+  protected
+
+  def initialize_inventory_collections
+    raise "initialize_inventory_collections must be defined in a subclass"
+  end
+
+  def add_inventory_collection(inventory_collection_data, key = nil)
+    model_class, data = inventory_collection_data
+    data[:parent] ||= ems
+
+    key ||= data[:association]
+    inventory_collections[key] = ::ManagerRefresh::InventoryCollection.new(model_class, data)
+  end
+
+  def event_payload(event)
+    transform_keys(event[:full_data]["configurationItem"]["configuration"])
+  end
+
+  def transform_keys(value)
+    case value
+    when Array
+      value.map { |x| transform_keys(x) }
+    when Hash
+      Hash[value.map { |k, v| [k.to_s.underscore, transform_keys(v)] }]
+    else
+      value
+    end
+  end
+end
