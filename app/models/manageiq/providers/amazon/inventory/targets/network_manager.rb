cf018628018f5340bf2507a7988e7d41d3f2814d
Ladislav Smola <lsmola@redhat.com>
2017-01-10 09:24:53 +0100

    NetworkManager target fix, typo in stacks name
    NetworkManager target fix, typo in stacks name

--- app/models/manageiq/providers/amazon/inventory/targets/network_manager.rb
+++ app/models/manageiq/providers/amazon/inventory/targets/network_manager.rb
@@ -6,7 +6,7 @@ class ManageIQ::Providers::Amazon::Inventory::Targets::NetworkManager < ManageIQ
          load_balancer_listeners load_balancer_listener_pools load_balancer_health_checks
          load_balancer_health_check_members))
 
-    add_inventory_collections(%i(vms orchestration_stack availability_zones),
+    add_inventory_collections(%i(vms orchestration_stacks availability_zones),
                               :parent   => ems.parent_manager,
                               :strategy => :local_db_cache_all)
   end
