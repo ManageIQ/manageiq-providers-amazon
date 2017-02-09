8e6f9d3bf0c4a324b57af589dca1742f776d434c
Ladislav Smola <lsmola@redhat.com>
2017-02-09 09:36:47 +0100

    Use base class InventoryCollection definitions
    Use base class InventoryCollection definitions, only adding
    AWS specific config.

---
+++ app/models/manageiq/providers/amazon/inventory_collection_default/cloud_manager.rb
@@ -0,0 +1,58 @@
+class ManageIQ::Providers::Amazon::InventoryCollectionDefault::CloudManager < ManagerRefresh::InventoryCollectionDefault::CloudManager
+  class << self
+    def vms(extra_attributes = {})
+      attributes = {
+        :model_class => ::ManageIQ::Providers::Amazon::CloudManager::Vm,
+      }
+      super(attributes.merge!(extra_attributes))
+    end
+
+    def miq_templates(extra_attributes = {})
+      attributes = {
+        :model_class => ::ManageIQ::Providers::Amazon::CloudManager::Template,
+      }
+
+      super(attributes.merge!(extra_attributes))
+    end
+
+    def availability_zones(extra_attributes = {})
+      attributes = {
+        :model_class => ::ManageIQ::Providers::Amazon::CloudManager::AvailabilityZone,
+      }
+
+      super(attributes.merge!(extra_attributes))
+    end
+
+    def flavors(extra_attributes = {})
+      attributes = {
+        :model_class => ::ManageIQ::Providers::Amazon::CloudManager::Flavor,
+      }
+
+      super(attributes.merge!(extra_attributes))
+    end
+
+    def key_pairs(extra_attributes = {})
+      attributes = {
+        :model_class => ::ManageIQ::Providers::Amazon::CloudManager::AuthKeyPair,
+      }
+
+      super(attributes.merge!(extra_attributes))
+    end
+
+    def orchestration_stacks(extra_attributes = {})
+      attributes = {
+        :model_class => ::ManageIQ::Providers::Amazon::CloudManager::OrchestrationStack,
+      }
+
+      super(attributes.merge!(extra_attributes))
+    end
+
+    def orchestration_templates(extra_attributes = {})
+      attributes = {
+        :model_class       => ::OrchestrationTemplateCfn,
+      }
+
+      super(attributes.merge!(extra_attributes))
+    end
+  end
+end
