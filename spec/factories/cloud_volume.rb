fe5147c4563dd0eec65e3a2e66784fe1c59792df
Gregor Berginc <gregor.berginc@xlab.si>
2017-03-13 23:04:37 +0100

    Enable cloud volume modifications
    Recently Amazon introduced a capability to modify existing cloud volumes
    on the fly. Depending on the volume type size, volume type and IOPS can
    be modified without detaching the volume first.
    
    This patch enhances the existing `update_volume` method that previously
    allowed changing only the name of the volume by using the new method
    `#modify_volume` from the aws-sdk gem. The patch also adds a set of
    tests verifying different possible options passed to the
    `update_volume`.

--- spec/factories/cloud_volume.rb
+++ spec/factories/cloud_volume.rb
@@ -1,4 +1,24 @@
 FactoryGirl.define do
   factory :cloud_volume_amazon, :parent => :cloud_volume,
                                 :class  => "ManageIQ::Providers::Amazon::StorageManager::Ebs::CloudVolume"
+  factory :cloud_volume_amazon_standard,
+          :parent => :cloud_volume,
+          :class  => "ManageIQ::Providers::Amazon::StorageManager::Ebs::CloudVolume" do
+    size 1.gigabyte
+    volume_type 'standard'
+  end
+
+  factory :cloud_volume_amazon_gp2,
+          :parent => :cloud_volume,
+          :class  => "ManageIQ::Providers::Amazon::StorageManager::Ebs::CloudVolume" do
+    size 1.gigabyte
+    volume_type 'gp2'
+  end
+
+  factory :cloud_volume_amazon_io1,
+          :parent => :cloud_volume,
+          :class  => "ManageIQ::Providers::Amazon::StorageManager::Ebs::CloudVolume" do
+    size 4.gigabytes
+    volume_type 'io1'
+  end
 end
