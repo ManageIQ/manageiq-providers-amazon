4991c16b341cc27acaa7e7d4e96d56b5e2cf33ed
Ladislav Smola <lsmola@redhat.com>
2017-04-26 15:04:22 +0200

    Cleanup storage managers parsers
    Cleanup storage managers parsers

--- app/models/manageiq/providers/amazon/inventory/parser/storage_manager/ebs.rb
+++ app/models/manageiq/providers/amazon/inventory/parser/storage_manager/ebs.rb
@@ -16,13 +16,9 @@ class ManageIQ::Providers::Amazon::Inventory::Parser::StorageManager::Ebs < Mana
 
   def volumes
     collector.cloud_volumes.each do |volume|
-      uid = volume['volume_id']
-
-      persister_volume = persister.cloud_volumes.find_or_build(uid)
-      persister_volume.assign_attributes(
+      persister_volume = persister.cloud_volumes.find_or_build(volume['volume_id']).assign_attributes(
         :ext_management_system => ems,
-        :ems_ref               => uid,
-        :name                  => get_from_tags(volume, :name) || uid,
+        :name                  => get_from_tags(volume, :name) || volume['volume_id'],
         :status                => volume['state'],
         :creation_time         => volume['create_time'],
         :volume_type           => volume['volume_type'],
@@ -31,19 +27,15 @@ class ManageIQ::Providers::Amazon::Inventory::Parser::StorageManager::Ebs < Mana
         :availability_zone     => persister.availability_zones.lazy_find(volume['availability_zone'])
       )
 
-      link_volume_to_disk(persister_volume, volume['attachments'])
+      volume_attachments(persister_volume, volume['attachments'])
     end
   end
 
   def snapshots
     collector.cloud_volume_snapshots.each do |snap|
-      uid = snap['snapshot_id']
-
-      persister_snapshot = persister.cloud_volume_snapshots.find_or_build(uid)
-      persister_snapshot.assign_attributes(
+      persister.cloud_volume_snapshots.find_or_build(snap['snapshot_id']).assign_attributes(
         :ext_management_system => ems,
-        :ems_ref               => uid,
-        :name                  => get_from_tags(snap, :name) || uid,
+        :name                  => get_from_tags(snap, :name) || snap['snapshot_id'],
         :status                => snap['state'],
         :creation_time         => snap['start_time'],
         :description           => snap['description'],
@@ -53,12 +45,7 @@ class ManageIQ::Providers::Amazon::Inventory::Parser::StorageManager::Ebs < Mana
     end
   end
 
-  # Overridden helper methods, we should put them in helper once we get rid of old refresh
-  def get_from_tags(resource, item)
-    (resource['tags'] || []).detect { |tag, _| tag['key'].downcase == item.to_s.downcase }.try(:[], 'value')
-  end
-
-  def link_volume_to_disk(persister_volume, attachments)
+  def volume_attachments(persister_volume, attachments)
     (attachments || []).each do |a|
       if a['device'].blank?
         log_header = "MIQ(#{self.class.name}.#{__method__}) Collecting data for EMS name: [#{ems.name}] id: [#{ems.id}]"
@@ -69,11 +56,14 @@ class ManageIQ::Providers::Amazon::Inventory::Parser::StorageManager::Ebs < Mana
 
       dev = File.basename(a['device'])
 
-      disk = persister.disks.find_or_build_by(:hardware    => persister.hardwares.lazy_find(a["instance_id"]),
-                                              :device_name => dev)
-      disk.location = dev
-      disk.size     = persister_volume.size
-      disk.backing  = persister_volume
+      persister.disks.find_or_build_by(
+        :hardware    => persister.hardwares.lazy_find(a["instance_id"]),
+        :device_name => dev
+      ).assign_attributes(
+        :location => dev,
+        :size     => persister_volume.size,
+        :backing  => persister_volume,
+      )
     end
   end
 end
