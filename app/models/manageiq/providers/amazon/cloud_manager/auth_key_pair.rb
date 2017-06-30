c204c605c1e88be1301c6cd6cbf0b1bf920ae1e3
hsong-rh <hsong@redhat.com>
2017-06-30 10:11:20 -0400

    Enable to save key_pair in Authentication

--- app/models/manageiq/providers/amazon/cloud_manager/auth_key_pair.rb
+++ app/models/manageiq/providers/amazon/cloud_manager/auth_key_pair.rb
@@ -1,2 +1,37 @@
 class ManageIQ::Providers::Amazon::CloudManager::AuthKeyPair < ManageIQ::Providers::CloudManager::AuthKeyPair
+  def self.raw_create_key_pair(ext_management_system, create_options)
+    ec2 = ext_management_system.connect
+    kp = ec2.create_key_pair(create_options)
+    ost = OpenStruct.new
+    ost.name = ost.key_name = kp.name
+    ost.fingerprint = kp.key_fingerprint
+    ost.private_key = kp.key_material
+    ost
+  rescue => err
+    _log.error "keypair=[#{name}], error: #{err}"
+    raise MiqException::Error, err.to_s, err.backtrace
+  end
+
+  def self.validate_create_key_pair(ext_management_system, _options = {})
+    if ext_management_system
+      {:available => true, :message => nil}
+    else
+      {:available => false,
+       :message   => _("The Keypair is not connected to an active %{table}") %
+         {:table => ui_lookup(:table => "ext_management_system")}}
+    end
+  end
+
+  def raw_delete_key_pair
+    ec2 = resource.connect
+    kp = ec2.key_pair(name)
+    kp.delete
+  rescue => err
+    _log.error "keypair=[#{name}], error: #{err}"
+    raise MiqException::Error, err.to_s, err.backtrace
+  end
+
+  def validate_delete_key_pair
+    {:available => true, :message => nil}
+  end
 end
