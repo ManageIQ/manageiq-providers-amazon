b4327143402842d92a016392a8d25a7354064425
Jerry Keselman <jerryk@redhat.com>
2017-07-14 13:41:09 -0400

    Fix RuboCop Warnings

--- app/models/manageiq/providers/amazon/cloud_manager/vm_or_template_shared/scanning.rb
+++ app/models/manageiq/providers/amazon/cloud_manager/vm_or_template_shared/scanning.rb
@@ -78,19 +78,19 @@ module ManageIQ::Providers::Amazon::CloudManager::VmOrTemplateShared::Scanning
     status        = scan_message         = "OK"
     status_code   = categories_processed = 0
     ost.xml_class = XmlHash::Document
-    bb            = last_err             = nil
+    bb            = last_err = nil
 
     xml_summary = ost.xml_class.createDoc(:summary)
     xml_node    = xml_node_scan = xml_summary.root.add_element("scanmetadata")
     xml_summary.root.add_attributes("taskid" => ost.taskid)
 
     data_dir = File.expand_path(Rails.root.join("data/metadata"))
-    _log.debug "creating #{data_dir}"
+    _log.debug("creating #{data_dir}")
     begin
       Dir.mkdir(data_dir)
     rescue Errno::EEXIST
       # Ignore if the directory was created by another thread
-      _log.debug "No need to create directory #{data_dir} since it exists."
+      _log.debug("No need to create directory #{data_dir} since it exists.")
     end unless File.exist?(data_dir)
     ost.skipConfig = true
     ost.config     = OpenStruct.new(
@@ -100,9 +100,9 @@ module ManageIQ::Providers::Amazon::CloudManager::VmOrTemplateShared::Scanning
 
     begin
       require 'blackbox/VmBlackBox'
-      _log.debug "instantiating BlackBox"
+      _log.debug("instantiating BlackBox")
       bb = Manageiq::BlackBox.new(guid, ost)
-      _log.debug "instantiated BlackBox"
+      _log.debug("instantiated BlackBox")
       categories = ost.category
       #
       # TODO: The amazon_ssa_support gem defines the category as an array,
@@ -116,9 +116,9 @@ module ManageIQ::Providers::Amazon::CloudManager::VmOrTemplateShared::Scanning
         st = Time.current
         xml = MIQRexml.load(ost.reply[:categories][c.to_sym])
         next unless xml
-        _log.debug "Writing scanned data to XML for [#{c}] to blackbox."
+        _log.debug("Writing scanned data to XML for [#{c}] to blackbox.")
         bb.saveXmlData(xml, c)
-        _log.debug "Writing XML complete."
+        _log.debug("Writing XML complete.")
 
         category_node = xml_summary.class.load(xml.root.shallow_copy.to_xml.to_s).root
         category_node.add_attributes("start_time" => st.utc.iso8601, "end_time" => Time.now.utc.iso8601)
@@ -127,17 +127,17 @@ module ManageIQ::Providers::Amazon::CloudManager::VmOrTemplateShared::Scanning
       end
     rescue NoMethodError => scan_err
       last_err = scan_err
-      _log.error "perform_metadata_sync Error - [#{scan_err}]"
-      _log.error "perform_metadata_sync Error - [#{scan_err.backtrace.join("\n")}]"
+      _log.error("perform_metadata_sync Error - [#{scan_err}]")
+      _log.error("perform_metadata_sync Error - [#{scan_err.backtrace.join("\n")}]")
     ensure
       bb.close if bb
       update_job_message(ost, "Scanning completed.")
       if last_err
         status = "Error"
-        status_code = 8
-	status_code = 16 if categories_processed.zero?
+        status_code  = 8
+        status_code  = 16 if categories_processed.zero?
         scan_message = last_err.to_s
-        _log.error "ScanMetadata error status:[#{status_code}]:  message:[#{last_err}]"
+        _log.error("ScanMetadata error status:[#{status_code}]:  message:[#{last_err}]")
         _log.debug { last_err.backtrace.join("\n") }
       end
 
@@ -148,7 +148,7 @@ module ManageIQ::Providers::Amazon::CloudManager::VmOrTemplateShared::Scanning
         "message"     => scan_message
       )
       save_metadata_op(MIQEncode.encode(xml_summary.to_xml.to_s), "b64,zlib,xml", ost.taskid)
-      _log.info "Completed: Sending scan summary to server.  TaskId:[#{ost.taskid}]  target:[#{name}]"
+      _log.info("Completed: Sending scan summary to server.  TaskId:[#{ost.taskid}]  target:[#{name}]")
     end
     sync_stashed_metadata(ost)
   end
