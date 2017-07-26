f8e278b4b6a8223770e576489d9056742f81b94c
hsong-rh <hsong@redhat.com>
2017-07-26 11:25:21 -0400

    Changes based on PR comments

--- app/models/manageiq/providers/amazon/cloud_manager/ssa_agent_manager.rb
+++ app/models/manageiq/providers/amazon/cloud_manager/ssa_agent_manager.rb
@@ -48,7 +48,7 @@ module ManageIQ::Providers::Amazon::CloudManager::SsaAgentManager
   end
 
   def ssa_bucket
-    config[:aws_ssa_bucket] || AmazonSsaSupport::DEFAULT_BUCKET_PREFIX + '-' + ext_management_system.guid
+    (config[:aws_ssa_bucket] || AmazonSsaSupport::DEFAULT_BUCKET_PREFIX) + '-' + ext_management_system.guid
   end
 
   def request_queue
@@ -68,7 +68,7 @@ module ManageIQ::Providers::Amazon::CloudManager::SsaAgentManager
   end
 
   def profile_name
-    "MIQ_SSA"
+    MIQ_SSA
   end
 
   # check timestamp of heartbeat of agent_id, return true if the last beat time in
@@ -211,38 +211,24 @@ module ManageIQ::Providers::Amazon::CloudManager::SsaAgentManager
         vpc_id: ec2.client.describe_vpcs.vpcs[0].vpc_id
       })
 
-      security_group.authorize_ingress({
-        ip_permissions: [{
-          ip_protocol: 'tcp',
-          from_port: 22,
-          to_port: 22,
-          ip_ranges: [{
-            cidr_ip: '0.0.0.0/0'
-          }]}]
-      })
-
-      security_group.authorize_ingress({
-        ip_permissions: [{
-          ip_protocol: 'tcp',
-          from_port: 80,
-          to_port: 80,
-          ip_ranges: [{
-            cidr_ip: '0.0.0.0/0'
-          }]}]
-      })
-
-      security_group.authorize_ingress({
-        ip_permissions: [{
-          ip_protocol: 'tcp',
-          from_port: 443,
-          to_port: 443,
-          ip_ranges: [{
-            cidr_ip: '0.0.0.0/0'
-          }]}]
-      })
+      sg_authorize_ingress(security_group, 'tcp', 22, 22)
+      sg_authorize_ingress(security_group, 'tcp', 80, 80)
+      sg_authorize_ingress(security_group, 'tcp', 443, 443)
 
       security_group.group_id
     end
+  end
+
+  def sg_authorize_ingress(sg, protocal, from_port, to_port)
+      sg.authorize_ingress({
+        ip_permissions: [{
+          ip_protocol: protocol,
+          from_port: from_port,
+          to_port: to_port,
+          ip_ranges: [{
+            cidr_ip: '0.0.0.0/0'
+          }]}]
+      })
   end
 
   # Get Key Pair for SSH. Create a new one if not exists. 
@@ -256,63 +242,87 @@ module ManageIQ::Providers::Amazon::CloudManager::SsaAgentManager
 
   def create_pem_file(pair_name = MIQ_SSA)
     kp = get_key_pair(pair_name)
-    pem_file_name = pair_name+".pem"
+    pem_file_name = pair_name + ".pem"
     File.open(pem_file_name, 'w') {|f| f.write(kp.auth_key) }
     File.chmod(0400, pem_file_name)
     pem_file_name
   end
 
-  def agent_startup_script(script_name = '/tools/amazon_ssa_extract.rb')
-    ssa_root = Gem.loaded_specs['amazon_ssa_support'].full_gem_path
-    ssa_script = ssa_root + script_name
-    ssa_cmd = "ruby #{ssa_script}"
+  def setup_ruby(log = '/var/log/miq_ssa_deploy.log')
+    <<~HEREDOC
+      yum -y update > #{log} 2>&1
+      yum -y install git-core zlib zlib-devel gcc-c++ patch readline readline-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison curl sqlite-devel postgresql-devel >> #{log} 2>&1
+      git clone https://github.com/rbenv/rbenv.git ~/.rbenv >> #{log} 2>&1
+      export HOME="/root" >> #{log} 2>&1
+      echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+      echo 'eval "$(/root/.rbenv/bin/rbenv init -)"' >> ~/.bash_profile
+      source ~/.bash_profile >> #{log} 2>&1
+      git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build >> #{log} 2>&1
+      echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
+      source ~/.bash_profile >> #{log} 2>&1
+      echo $PATH >> #{log} 2>&1
+      rbenv install -l >> #{log} 2>&1
+      rbenv install 2.3.3 >> #{log} 2>&1
+      rbenv global 2.3.3 >> #{log} 2>&1
+      gem install bundler >> #{log} 2>&1
+      gem install rails >> #{log} 2>&1
+      gem install aws-sdk >> #{log} 2>&1
+      rbenv rehash >> #{log} 2>&1
+      ruby -v >> #{log} 2>&1
+    HEREDOC
   end
 
   def default_settings
-    "echo -e '---\n:log_level: #{log_level}\n:region: #{region}\n:request_queue: #{request_queue}\n:reply_queue: #{reply_queue}\n:ssa_bucket: #{ssa_bucket}\n:reply_prefix: #{reply_prefix}\n:log_prefix: #{log_prefix}\n:heartbeat_prefix: #{heartbeat_prefix}\n:heartbeat_interval: i#{heartbeat_interval}\n' > default_ssa_config.yml"
+    <<~HEREDOC
+      echo "---" > default_ssa_config.yml
+      echo ":log_level: #{log_level}" >> default_ssa_config.yml
+      echo ":region: #{region}" >> default_ssa_config.yml
+      echo ":request_queue: #{request_queue}" >> default_ssa_config.yml
+      echo ":reply_queue: #{reply_queue}" >> default_ssa_config.yml
+      echo ":ssa_bucket: #{ssa_bucket}" >> default_ssa_config.yml
+      echo ":reply_prefix: #{reply_prefix}" >> default_ssa_config.yml
+      echo ":log_prefix: #{log_prefix}" >> default_ssa_config.yml
+      echo ":heartbeat_prefix: #{heartbeat_prefix}" >> default_ssa_config.yml
+      echo ":heartbeat_interval: #{heartbeat_interval}" >> default_ssa_config.yml
+    HEREDOC
   end
 
-  def create_user_data
-    script = '#!/bin/bash
-yum -y update > /var/log/miq_ssa_deploy.log 2>&1
-yum -y install git-core zlib zlib-devel gcc-c++ patch readline readline-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison curl sqlite-devel postgresql-devel >> /var/log/miq_ssa_deploy.log 2>&1
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv >> /var/log/miq_ssa_deploy.log 2>&1
-export HOME="/root" >> /var/log/miq_ssa_deploy.log 2>&1
-echo \'export PATH="$HOME/.rbenv/bin:$PATH"\' >> ~/.bash_profile
-echo \'eval "$(/root/.rbenv/bin/rbenv init -)"\' >> ~/.bash_profile
-source ~/.bash_profile >> /var/log/miq_ssa_deploy.log 2>&1
-git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build >> /var/log/miq_ssa_deploy.log 2>&1
-echo \'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"\' >> ~/.bash_profile
-source ~/.bash_profile >> /var/log/miq_ssa_deploy.log 2>&1
-echo $PATH >> /var/log/miq_ssa_deploy.log 2>&1
-rbenv install -l >> /var/log/miq_ssa_deploy.log 2>&1
-rbenv install 2.3.3 >> /var/log/miq_ssa_deploy.log 2>&1
-rbenv global 2.3.3 >> /var/log/miq_ssa_deploy.log 2>&1
-gem install bundler >> /var/log/miq_ssa_deploy.log 2>&1
-gem install rails >> /var/log/miq_ssa_deploy.log 2>&1
-gem install aws-sdk >> /var/log/miq_ssa_deploy.log 2>&1
-rbenv rehash >> /var/log/miq_ssa_deploy.log 2>&1
-ruby -v >> /var/log/miq_ssa_deploy.log 2>&1
-mkdir -p /opt/miq/log
-cd /opt/miq
-echo \'source "https://rubygems.org"
-gem "manageiq-gems-pending", ">0", :require => "manageiq-gems-pending", :git => "https://github.com/ManageIQ/manageiq-gems-pending.git", :branch => "master"
-gem "manageiq-smartstate", ">0", :require => "manageiq-smartstate", :git => "https://github.com/ManageIQ/manageiq-smartstate.git", :branch => "master"
-gem "amazon_ssa_support", ">0", :require => "amazon_ssa_support", :git => "https://github.com/ManageIQ/amazon_ssa_support.git", :branch => "master"
-# Modified gems for gems-pending.  Setting sources here since they are git references
-gem "handsoap", "~>0.2.5", :require => false, :git => "https://github.com/ManageIQ/handsoap.git", :tag => "v0.2.5-5"
-gem "aws-sdk"\' > Gemfile
-bundle install >> /var/log/miq_ssa_deploy.log 2>&1
-' + default_settings + '
-echo \'#!/bin/sh\' > start_agent.sh
-echo \'ssa_root=`bundle show amazon_ssa_support`\' >> start_agent.sh
-echo \'ssa_script="$ssa_root/tools/amazon_ssa_extract.rb"\' >> start_agent.sh
-echo \'ruby ${ssa_script}\' >> start_agent.sh
-chmod 755 start_agent.sh
-echo "Agent starts ...." >> /var/log/miq_ssa_deploy.log 2>&1
-./start_agent.sh
-'
-    Base64.encode64(script)
+  def github_gem_file
+    <<~HEREDOC
+      echo 'source "https://rubygems.org"' > Gemfile
+      echo 'gem "manageiq-gems-pending", ">0", :require => "manageiq-gems-pending", :git => "https://github.com/ManageIQ/manageiq-gems-pending.git", :branch => "master"' >> Gemfile
+      echo 'gem "manageiq-smartstate", ">0", :require => "manageiq-smartstate", :git => "https://github.com/ManageIQ/manageiq-smartstate.git", :branch => "master"' >> Gemfile
+      echo 'gem "amazon_ssa_support", ">0", :require => "amazon_ssa_support", :git => "https://github.com/ManageIQ/amazon_ssa_support.git", :branch => "master"' >> Gemfile
+      # Modified gems for gems-pending.  Setting sources here since they are git references
+      echo 'gem "handsoap", "~>0.2.5", :require => false, :git => "https://github.com/ManageIQ/handsoap.git", :tag => "v0.2.5-5"' >> Gemfile
+      echo 'gem "aws-sdk"' >> Gemfile
+    HEREDOC
+  end
+
+  def agent_startup_script(log = '/var/log/miq_ssa_deploy.log')
+    <<~HEREDOC
+      echo "#!/bin/sh" > start_agent.sh
+      echo "ssa_root=`bundle show amazon_ssa_support`" >> start_agent.sh
+      echo 'ssa_script="$ssa_root/tools/amazon_ssa_extract.rb"' >> start_agent.sh
+      echo 'ruby $ssa_script -l #{log_level}' >> start_agent.sh
+      chmod 755 start_agent.sh
+      echo "Agent starts ..." >> #{log} 2>&1
+      ./start_agent.sh
+    HEREDOC
+  end
+
+  def create_user_data(log = '/var/log/miq_ssa_deploy.log')
+    userdata = <<~HEREDOC
+      #!/bin/bash
+      #{setup_ruby}
+      mkdir -p /opt/miq/log
+      cd /opt/miq
+      #{github_gem_file}
+      bundle install >> #{log} 2>&1
+      #{default_settings} 
+      #{agent_startup_script}
+    HEREDOC
+    Base64.encode64(userdata)
   end
 
   def get_subnets(az)
