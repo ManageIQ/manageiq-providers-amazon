377bb3568b96c3be6a809b44cb43baaf94d705e1
Julian Cheal <jcheal@redhat.com>
2017-10-03 20:14:45 +0100

    Remove Amazon provider discovery
    Fixes BZ https://bugzilla.redhat.com/show_bug.cgi?id=1429382
    
    An Amazon discovery can end up overloading the system and causing
    more issues than it solves.
    
    This feature should hopefully be reimplemented in the future after
    the re-architecture.

--- app/models/manageiq/providers/amazon/manager_mixin.rb
+++ app/models/manageiq/providers/amazon/manager_mixin.rb
@@ -95,41 +95,6 @@ module ManageIQ::Providers::Amazon::ManagerMixin
       end
     end
 
-    #
-    # Discovery
-    #
-
-    # Factory method to create EmsAmazon instances for all regions with instances
-    #   or images for the given authentication.  Created EmsAmazon instances
-    #   will automatically have EmsRefreshes queued up.  If this is a greenfield
-    #   discovery, we will at least add an EmsAmazon for us-east-1
-    def discover(access_key_id, secret_access_key)
-      new_emses         = []
-      all_emses         = includes(:authentications)
-      all_ems_names     = all_emses.map(&:name).to_set
-      known_ems_regions = all_emses.select { |e| e.authentication_userid == access_key_id }.map(&:provider_region)
-
-      ec2 = raw_connect(access_key_id, secret_access_key, :EC2, "us-east-1")
-      region_names_to_discover = ec2.client.describe_regions.regions.map(&:region_name)
-
-      (region_names_to_discover - known_ems_regions).each do |region_name|
-        ec2_region = raw_connect(access_key_id, secret_access_key, :EC2, region_name)
-        next if ec2_region.instances.count == 0 && # instances
-                ec2_region.images(:owners => %w(self)).count == 0 && # private images
-                ec2_region.images(:executable_users => %w(self)).count == 0 # shared  images
-        new_emses << create_discovered_region(region_name, access_key_id, secret_access_key, all_ems_names)
-      end
-
-      # If greenfield Amazon, at least create the us-east-1 region.
-      if new_emses.blank? && known_ems_regions.blank?
-        new_emses << create_discovered_region("us-east-1", access_key_id, secret_access_key, all_ems_names)
-      end
-
-      EmsRefresh.queue_refresh(new_emses) unless new_emses.blank?
-
-      new_emses
-    end
-
     def discover_queue(access_key_id, secret_access_key)
       MiqQueue.put(
         :class_name  => name,
@@ -142,29 +107,6 @@ module ManageIQ::Providers::Amazon::ManagerMixin
 
     def discover_from_queue(access_key_id, secret_access_key)
       discover(access_key_id, MiqPassword.decrypt(secret_access_key))
-    end
-
-    def create_discovered_region(region_name, access_key_id, secret_access_key, all_ems_names)
-      name = region_name
-      name = "#{region_name} #{access_key_id}" if all_ems_names.include?(name)
-      while all_ems_names.include?(name)
-        name_counter = name_counter.to_i + 1 if defined?(name_counter)
-        name = "#{region_name} #{name_counter}"
-      end
-
-      new_ems = create!(
-        :name            => name,
-        :provider_region => region_name,
-        :zone            => Zone.default_zone
-      )
-      new_ems.update_authentication(
-        :default => {
-          :userid   => access_key_id,
-          :password => secret_access_key
-        }
-      )
-
-      new_ems
     end
   end
 end
