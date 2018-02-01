ee35c8d22fae339e207044bfc5c1f275b30ea247
Milan Zazrivec <mzazrivec@redhat.com>
2018-02-01 11:01:46 +0100

    Add display_name() for Network Router model

--- app/models/manageiq/providers/amazon/network_manager/network_router.rb
+++ app/models/manageiq/providers/amazon/network_manager/network_router.rb
@@ -1,2 +1,5 @@
 class ManageIQ::Providers::Amazon::NetworkManager::NetworkRouter < ::NetworkRouter
+  def self.display_name(number = 1)
+    n_('Network Router (Amazon)', 'Network Routers (Amazon)', number)
+  end
 end
