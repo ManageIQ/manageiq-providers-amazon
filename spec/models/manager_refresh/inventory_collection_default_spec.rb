75ef6b2be9ad2306420b4177746b59d6e37b56de
Alexander Zagaynov <azagayno@redhat.com>
2018-04-11 19:07:55 +0200

    test tagging redoubling on ems refresh

--- spec/models/manager_refresh/inventory_collection_default_spec.rb
+++ spec/models/manager_refresh/inventory_collection_default_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 describe ManagerRefresh::InventoryCollectionDefault do
+  let(:ems) { new_amazon_ems }
+
   it 'pickups existing Vm and MiqTemplate records on Amazon EMS re-adding' do
     ems = new_amazon_ems
 
@@ -24,6 +26,36 @@ describe ManagerRefresh::InventoryCollectionDefault do
     end
   end
 
+  context 'label tag mapping' do
+    before(:context) do
+      tag = Tag.find_or_create_by!(:name => '/managed/amazon:vm:some_tag')
+      Classification
+        .create_with(:example_text => 'Example Tag Mapping Classification (text)',
+                     :description  => 'Example tag mapping classification (description)')
+        .find_or_create_by!(:tag => tag)
+      ContainerLabelTagMapping
+        .find_or_create_by!(:tag                   => tag,
+                            :label_name            => 'some_tag',
+                            :labeled_resource_type => 'Vm')
+    end
+
+    it 'pickups existing items on refresh' do
+      with_vcr_data do
+        expect { EmsRefresh.refresh(ems) }
+          .to  change { CustomAttribute.count } .from(0)
+          .and change { Classification.count }  .from(1)
+          .and change { Tag.count }             .from(1)
+          .and change { Tagging.count }         .from(0)
+
+        expect { EmsRefresh.refresh(ems) }
+          .to  change { CustomAttribute.count } .by(0)
+          .and change { Classification.count }  .by(0)
+          .and change { Tag.count }             .by(0)
+          .and change { Tagging.count }         .by(0)
+      end
+    end
+  end
+
   private
 
   def new_amazon_ems
@@ -32,6 +64,6 @@ describe ManagerRefresh::InventoryCollectionDefault do
 
   # TODO: (zalex) DRY - put in the shared helpers
   def with_vcr_data
-    VCR.use_cassette(described_class.name.underscore) { yield }
+    VCR.use_cassette(described_class.name.underscore, :allow_unused_http_interactions => true) { yield }
   end
 end
