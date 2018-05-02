14721a16ff4dcf007b859cf34bd02fed4275d2e6
Martin Slemr <mslemr@redhat.com>
2018-05-02 14:58:04 +0200

    Removed included do from mixins

--- app/models/manageiq/providers/amazon/inventory/persister/shared/network_collections.rb
+++ app/models/manageiq/providers/amazon/inventory/persister/shared/network_collections.rb
@@ -1,26 +1,24 @@
 module ManageIQ::Providers::Amazon::Inventory::Persister::Shared::NetworkCollections
   extend ActiveSupport::Concern
 
-  included do
-    # Builder class for Network
-    def network
-      ::ManagerRefresh::InventoryCollection::Builder::NetworkManager
-    end
+  # Builder class for Network
+  def network
+    ::ManagerRefresh::InventoryCollection::Builder::NetworkManager
+  end
 
-    def add_cloud_subnet_network_ports(extra_properties = {})
-      add_collection(network, :cloud_subnet_network_ports, extra_properties) do |builder|
-        builder.add_properties(
-          :manager_ref_allowed_nil => %i(cloud_subnet)
-        )
-      end
+  def add_cloud_subnet_network_ports(extra_properties = {})
+    add_collection(network, :cloud_subnet_network_ports, extra_properties) do |builder|
+      builder.add_properties(
+        :manager_ref_allowed_nil => %i(cloud_subnet)
+      )
     end
+  end
 
-    def add_firewall_rules(extra_properties = {})
-      add_collection(network, :firewall_rules, extra_properties) do |builder|
-        builder.add_properties(
-          :manager_ref_allowed_nil => %i(source_security_group)
-        )
-      end
+  def add_firewall_rules(extra_properties = {})
+    add_collection(network, :firewall_rules, extra_properties) do |builder|
+      builder.add_properties(
+        :manager_ref_allowed_nil => %i(source_security_group)
+      )
     end
   end
 end
--- app/models/manageiq/providers/amazon/inventory/persister/shared/storage_collections.rb
+++ app/models/manageiq/providers/amazon/inventory/persister/shared/storage_collections.rb
@@ -1,58 +1,56 @@
 module ManageIQ::Providers::Amazon::Inventory::Persister::Shared::StorageCollections
   extend ActiveSupport::Concern
 
-  included do
-    # Builder class for Storage
-    def storage
-      ::ManagerRefresh::InventoryCollection::Builder::StorageManager
+  # Builder class for Storage
+  def storage
+    ::ManagerRefresh::InventoryCollection::Builder::StorageManager
+  end
+
+  def add_cloud_volumes(extra_properties = {})
+    add_collection(storage, :cloud_volumes, extra_properties) do |builder|
+      builder.add_properties(
+        :model_class => ::ManageIQ::Providers::Amazon::StorageManager::Ebs::CloudVolume
+      )
+
+      builder.add_builder_params(
+        :ems_id => ->(persister) { persister.manager.try(:ebs_storage_manager).try(:id) || persister.manager.id }
+      )
     end
+  end
 
-    def add_cloud_volumes(extra_properties = {})
-      add_collection(storage, :cloud_volumes, extra_properties) do |builder|
-        builder.add_properties(
-          :model_class => ::ManageIQ::Providers::Amazon::StorageManager::Ebs::CloudVolume
-        )
+  def add_cloud_volume_snapshots(extra_properties = {})
+    add_collection(storage, :cloud_volume_snapshots, extra_properties) do |builder|
+      builder.add_properties(
+        :model_class => ::ManageIQ::Providers::Amazon::StorageManager::Ebs::CloudVolumeSnapshot
+      )
 
-        builder.add_builder_params(
-          :ems_id => ->(persister) { persister.manager.try(:ebs_storage_manager).try(:id) || persister.manager.id }
-        )
-      end
+      builder.add_builder_params(
+        :ems_id => ->(persister) { persister.manager.try(:ebs_storage_manager).try(:id) || persister.manager.id }
+      )
     end
+  end
 
-    def add_cloud_volume_snapshots(extra_properties = {})
-      add_collection(storage, :cloud_volume_snapshots, extra_properties) do |builder|
-        builder.add_properties(
-          :model_class => ::ManageIQ::Providers::Amazon::StorageManager::Ebs::CloudVolumeSnapshot
-        )
+  def add_cloud_object_store_containers(extra_properties = {})
+    add_collection(storage, :cloud_object_store_containers, extra_properties) do |builder|
+      builder.add_properties(
+        :model_class => ::ManageIQ::Providers::Amazon::StorageManager::S3::CloudObjectStoreContainer
+      )
 
-        builder.add_builder_params(
-          :ems_id => ->(persister) { persister.manager.try(:ebs_storage_manager).try(:id) || persister.manager.id }
-        )
-      end
+      builder.add_builder_params(
+        :ems_id => ->(persister) { persister.manager.try(:s3_storage_manager).try(:id) || persister.manager.id }
+      )
     end
+  end
 
-    def add_cloud_object_store_containers(extra_properties = {})
-      add_collection(storage, :cloud_object_store_containers, extra_properties) do |builder|
-        builder.add_properties(
-          :model_class => ::ManageIQ::Providers::Amazon::StorageManager::S3::CloudObjectStoreContainer
-        )
+  def add_cloud_object_store_objects(extra_properties = {})
+    add_collection(storage, :cloud_object_store_objects, extra_properties) do |builder|
+      builder.add_properties(
+        :model_class => ::ManageIQ::Providers::Amazon::StorageManager::S3::CloudObjectStoreObject
+      )
 
-        builder.add_builder_params(
-          :ems_id => ->(persister) { persister.manager.try(:s3_storage_manager).try(:id) || persister.manager.id }
-        )
-      end
-    end
-
-    def add_cloud_object_store_objects(extra_properties = {})
-      add_collection(storage, :cloud_object_store_objects, extra_properties) do |builder|
-        builder.add_properties(
-          :model_class => ::ManageIQ::Providers::Amazon::StorageManager::S3::CloudObjectStoreObject
-        )
-
-        builder.add_builder_params(
-          :ems_id => ->(persister) { persister.manager.try(:s3_storage_manager).try(:id) || persister.manager.id }
-        )
-      end
+      builder.add_builder_params(
+        :ems_id => ->(persister) { persister.manager.try(:s3_storage_manager).try(:id) || persister.manager.id }
+      )
     end
   end
 end
