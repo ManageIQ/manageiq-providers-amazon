3eaf1efdab721a8e7423dfdc8d6afef319ea449e
Ladislav Smola <lsmola@redhat.com>
2018-06-11 13:15:39 +0200

    Replace vm start&stop by ansible call
    Replace vm start&stop by ansible call

--- app/models/manageiq/providers/amazon/cloud_manager/vm/operations/power.rb
+++ app/models/manageiq/providers/amazon/cloud_manager/vm/operations/power.rb
@@ -9,13 +9,19 @@ module ManageIQ::Providers::Amazon::CloudManager::Vm::Operations::Power
   end
 
   def raw_start
-    with_provider_object(&:start)
+    Ansible::Runner.run(ext_management_system.ansible_env_vars,
+                        {:instance_ids => ems_ref},
+                        ext_management_system.ansible_root.join("start_vm.yml"))
+
     # Temporarily update state for quick UI response until refresh comes along
     self.update_attributes!(:raw_power_state => "powering_up")
   end
 
   def raw_stop
-    with_provider_object(&:stop)
+    Ansible::Runner.run(ext_management_system.ansible_env_vars,
+                        {:instance_ids => ems_ref},
+                        ext_management_system.ansible_root.join("stop_vm.yml"))
+
     # Temporarily update state for quick UI response until refresh comes along
     self.update_attributes!(:raw_power_state => "shutting_down")
   end
