c801c637e3c8a95674a25d0f430af1ab7d494719
Ladislav Smola <lsmola@redhat.com>
2018-06-15 10:19:39 +0200

    Use string options instead of symbol, so it's API friendly

--- app/models/manageiq/providers/amazon/network_manager/security_group.rb
+++ app/models/manageiq/providers/amazon/network_manager/security_group.rb
@@ -8,11 +8,11 @@ class ManageIQ::Providers::Amazon::NetworkManager::SecurityGroup < ::SecurityGro
   def self.raw_create_security_group(ext_management_system, options)
     Ansible::Runner.run(ext_management_system.parent_manager.ansible_env_vars,
                         {
-                          :vpc_id                      => options[:vpc_id],
-                          :security_group_name         => options[:security_group_name],
-                          :security_group_description  => options[:security_group_description],
-                          :security_group_rules        => options[:security_group_rules],
-                          :security_group_rules_egress => options[:security_group_rules_egress],
+                          :vpc_id                      => options["vpc_id"],
+                          :security_group_name         => options["security_group_name"],
+                          :security_group_description  => options["security_group_description"],
+                          :security_group_rules        => options["security_group_rules"],
+                          :security_group_rules_egress => options["security_group_rules_egress"],
                         },
                         ext_management_system.parent_manager.ansible_root.join("create_security_group.yml"))
   rescue => e
