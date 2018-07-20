f6d0ed52c1943c771943504d9c89f8551c412ad0
Martin Maroši <marvusm.mmi@gmail.com>
2018-07-20 14:05:31 +0200

    Update react-ui-components version.

--- app/javascript/packs/component-definitions-common.js
+++ app/javascript/packs/component-definitions-common.js
@@ -1,3 +1,4 @@
 import CreateAmazonSecurityGroupForm from '../components/create-amazon-security-group-form';
+import '@manageiq/react-ui-components/dist/amazon-security-form-group.css';
 
 ManageIQ.component.addReact('CreateAmazonSecurityGroupForm', CreateAmazonSecurityGroupForm);
