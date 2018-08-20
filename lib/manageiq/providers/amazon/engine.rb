f4377702c496b9d9a8f08357270f231b93163533
Milan Zazrivec <mzazrivec@redhat.com>
2018-08-20 15:40:48 +0200

    Add plugin display name

--- lib/manageiq/providers/amazon/engine.rb
+++ lib/manageiq/providers/amazon/engine.rb
@@ -3,6 +3,10 @@ module ManageIQ
     module Amazon
       class Engine < ::Rails::Engine
         isolate_namespace ManageIQ::Providers::Amazon
+
+        def self.plugin_name
+          _('Amazon Provider')
+        end
       end
     end
   end
