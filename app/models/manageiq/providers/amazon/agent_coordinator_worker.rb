24eb987042cbc88e2f3e4764f5dd5d84e3ec2824
Nick Carboni <ncarboni@redhat.com>
2019-11-14 17:48:03 -0500

    Refactor the desired_queue_names method
    Don't re-implement logic from the base class

--- app/models/manageiq/providers/amazon/agent_coordinator_worker.rb
+++ app/models/manageiq/providers/amazon/agent_coordinator_worker.rb
@@ -10,10 +10,7 @@ class ManageIQ::Providers::Amazon::AgentCoordinatorWorker < MiqWorker
   end
 
   def self.desired_queue_names
-    return [] if MiqServer.minimal_env? && !self.has_minimal_env_option?
-    cloud_managers = all_valid_ems_in_zone.collect { |e| e.kind_of?(ems_class) }
-
     # All cloud managers will share the same agent coordinator
-    cloud_managers.any? ? ["ems_agent_coordinator"] : []
+    super.any? ? ["ems_agent_coordinator"] : []
   end
 end
