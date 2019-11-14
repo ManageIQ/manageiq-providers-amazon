d9e342fae6f966118c19f5fdefc05cec70cfa2e8
Nick Carboni <ncarboni@redhat.com>
2019-11-14 17:46:01 -0500

    Define ManageIQ::Providers::Amazon::AgentCoordinatorWorker.ems_class
    Also create some specs to validate the desired_queue_names behavior

--- app/models/manageiq/providers/amazon/agent_coordinator_worker.rb
+++ app/models/manageiq/providers/amazon/agent_coordinator_worker.rb
@@ -5,9 +5,13 @@ class ManageIQ::Providers::Amazon::AgentCoordinatorWorker < MiqWorker
 
   self.required_roles = ['smartproxy']
 
+  def self.ems_class
+    ManageIQ::Providers::Amazon::CloudManager
+  end
+
   def self.desired_queue_names
     return [] if MiqServer.minimal_env? && !self.has_minimal_env_option?
-    cloud_managers = all_valid_ems_in_zone.collect { |e| e.kind_of?(ManageIQ::Providers::Amazon::CloudManager) }
+    cloud_managers = all_valid_ems_in_zone.collect { |e| e.kind_of?(ems_class) }
 
     # All cloud managers will share the same agent coordinator
     cloud_managers.any? ? ["ems_agent_coordinator"] : []
