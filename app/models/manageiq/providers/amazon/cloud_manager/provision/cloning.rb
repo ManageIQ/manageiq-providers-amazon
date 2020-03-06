ca0d2e065478d340142d5c9d48f4b83fd4d037e1
Andrew Becker <anbecker@redhat.com>
2020-03-06 12:30:27 -0500

    Addressing review comments for PR #604

--- app/models/manageiq/providers/amazon/cloud_manager/provision/cloning.rb
+++ app/models/manageiq/providers/amazon/cloud_manager/provision/cloning.rb
@@ -2,13 +2,11 @@ module ManageIQ::Providers::Amazon::CloudManager::Provision::Cloning
   def do_clone_task_check(clone_task_ref)
     source.with_provider_connection(:sdk_v2 => true) do |ec2|
       instance = ec2.instance(clone_task_ref)
-      if instance.exists?
-        status   = instance.state.name.to_sym
-        return true if status == :running
-        return false, status
-      else
-        return false, :pending
-      end
+      return false, :pending unless instance.exists?
+
+      status = instance.state.name.to_sym
+      return true if status == :running
+      return false, status
     end
   end
 
