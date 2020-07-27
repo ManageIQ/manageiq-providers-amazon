7ff8c58708dcf221ef38f2a21adf1ea94f9407ff
Daniel Berger <djberg96@gmail.com>
2020-07-27 16:34:17 -0400

    Modify get_from_tags to collect tag_set information as well.

--- app/models/manageiq/providers/amazon/inventory/parser.rb
+++ app/models/manageiq/providers/amazon/inventory/parser.rb
@@ -7,6 +7,7 @@ class ManageIQ::Providers::Amazon::Inventory::Parser < ManageIQ::Providers::Inve
   # Overridden helper methods, we should put them in helper once we get rid of old refresh
   def get_from_tags(resource, tag_name)
     tag_name = tag_name.to_s.downcase
-    Array.wrap(resource['tags']).detect { |tag, _| tag['key'].downcase == tag_name }.try(:[], 'value').presence
+    tags = resource['tags'].to_a.concat(resource['tag_set'].to_a)
+    Array.wrap(tags).detect { |tag, _| tag['key'].downcase == tag_name }.try(:[], 'value').presence
   end
 end
