f1af6ddc6e713b5cf8710f0ebdc8ebed3c9bea08
Daniel Berger <djberg96@gmail.com>
2020-12-16 08:38:29 -0500

    Disable useless assignment cop in manager mixin.

--- app/models/manageiq/providers/amazon/manager_mixin.rb
+++ app/models/manageiq/providers/amazon/manager_mixin.rb
@@ -76,6 +76,7 @@ module ManageIQ::Providers::Amazon::ManagerMixin
       # Pull out the password from the database if a provider ID is available
       secret_access_key ||= find(args["id"]).authentication_password('default')
 
+      # rubocop:disable Lint/UselessAssignment (see #588)
       !!raw_connect(access_key, secret_access_key, :EC2, region, proxy_uri, validate = true, :assume_role => service_account)
     end
 
