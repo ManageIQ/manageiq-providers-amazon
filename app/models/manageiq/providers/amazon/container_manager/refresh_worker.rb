008d00a719f1960de187fa34fa73650bbfd0b08d
Adam Grare <agrare@redhat.com>
2021-02-04 12:19:26 -0500

    Subclass RefreshWorker::WatchThread

--- app/models/manageiq/providers/amazon/container_manager/refresh_worker.rb
+++ app/models/manageiq/providers/amazon/container_manager/refresh_worker.rb
@@ -1,5 +1,6 @@
 class ManageIQ::Providers::Amazon::ContainerManager::RefreshWorker < ManageIQ::Providers::BaseManager::RefreshWorker
   require_nested :Runner
+  require_nested :WatchThread
 
   def self.settings_name
     :ems_refresh_worker_amazon_eks
---
+++ app/models/manageiq/providers/amazon/container_manager/refresh_worker/watch_thread.rb
@@ -0,0 +1,2 @@
+class ManageIQ::Providers::Amazon::ContainerManager::RefreshWorker::WatchThread < ManageIQ::Providers::Kubernetes::ContainerManager::RefreshWorker::WatchThread
+end
