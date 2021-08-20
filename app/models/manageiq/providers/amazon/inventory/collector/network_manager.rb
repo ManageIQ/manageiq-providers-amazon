01e5f9f1949b5ef9e4d6fcd73cdd17604ee22c09
Adam Grare <adam@grare.com>
2021-08-20 14:39:33 -0400

    Prevent optional services from causing failures
    Only access to the EC2 service is required for the refresh to complete,
    other services are optional and failures there (e.g. due to lack of
    permissions) shouldn't cause the entire refresh to fail.

--- app/models/manageiq/providers/amazon/inventory/collector/network_manager.rb
+++ app/models/manageiq/providers/amazon/inventory/collector/network_manager.rb
@@ -17,12 +17,20 @@ class ManageIQ::Providers::Amazon::Inventory::Collector::NetworkManager < Manage
 
   def load_balancers
     hash_collection.new(aws_elb.client.describe_load_balancers.flat_map(&:load_balancer_descriptions))
+  rescue
+    # ELB is an optional service and failures shouldn't prevent the rest
+    # of the refresh from succeeding
+    []
   end
 
   def health_check_members(load_balancer_name)
     hash_collection.new(aws_elb.client.describe_instance_health(
       :load_balancer_name => load_balancer_name
     ).flat_map(&:instance_states))
+  rescue
+    # ELB is an optional service and failures shouldn't prevent the rest
+    # of the refresh from succeeding
+    []
   end
 
   def floating_ips
