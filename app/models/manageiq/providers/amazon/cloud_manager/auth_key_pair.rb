073d23c8b5f48b571ec56b5e2615ae3844c6f475
Oleg Barenboim <chessbyte@gmail.com>
2021-09-24 17:07:45 -0400

    Convert validate_create_key_pair to use ems.supports?(:auth_key_pair_create)

--- app/models/manageiq/providers/amazon/cloud_manager/auth_key_pair.rb
+++ app/models/manageiq/providers/amazon/cloud_manager/auth_key_pair.rb
@@ -17,16 +17,6 @@ class ManageIQ::Providers::Amazon::CloudManager::AuthKeyPair < ManageIQ::Provide
     raise MiqException::Error, err.to_s, err.backtrace
   end
 
-  def self.validate_create_key_pair(ext_management_system, _options = {})
-    if ext_management_system
-      {:available => true, :message => nil}
-    else
-      {:available => false,
-       :message   => _("The Keypair is not connected to an active %{table}") %
-         {:table => ui_lookup(:table => "ext_management_system")}}
-    end
-  end
-
   def raw_delete_key_pair
     ec2 = resource.connect
     kp = ec2.key_pair(name)
