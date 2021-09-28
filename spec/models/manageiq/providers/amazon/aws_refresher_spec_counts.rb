34bd09b0f6030855d37399f6df10a61057515177
Jason Frey <fryguy9@gmail.com>
2021-09-28 13:14:55 -0400

    Merge pull request #723 from agrare/fix_provider_tag_mapping
    Fix labeled_resource_type for Amazon Vms/Images
    
    (cherry picked from commit 958218753db632e2fdf21bb1db9ce2c8a4023f5e)

--- spec/models/manageiq/providers/amazon/aws_refresher_spec_counts.rb
+++ spec/models/manageiq/providers/amazon/aws_refresher_spec_counts.rb
@@ -65,15 +65,11 @@ module AwsRefresherSpecCounts
     # Custom attributes of all Vms and Images
     custom_attributes_count = (vms_tags + images_tags).size
 
-    vms_mappings = ProviderTagMapping.where(:labeled_resource_type => nil).or(
-      ProviderTagMapping.where(:labeled_resource_type => "Vm")
-    ).pluck(:label_name)
+    vms_mappings = ProviderTagMapping.where(:labeled_resource_type => ["_all_entities_", "VmAmazon"]).pluck(:label_name)
     # Correct count of mapped tags for Vms
     vms_tags_count = vms_tags.select { |x| vms_mappings.include?(x["key"]) && x["value"].present? }.count
 
-    images_mappings = ProviderTagMapping.where(:labeled_resource_type => nil).or(
-      ProviderTagMapping.where(:labeled_resource_type => "Image")
-    ).pluck(:label_name)
+    images_mappings = ProviderTagMapping.where(:labeled_resource_type => ["_all_entities_", "ImageAmazon"]).pluck(:label_name)
     # Correct count of mapped tags for Images
     images_tags_count = images_tags.select { |x| images_mappings.include?(x["key"]) && x["value"].present? }.count
 
