0ee0efad7a50990a522664f30641fd72c63f9b46
Adam Grare <adam@grare.com>
2022-01-11 10:23:06 -0500

    Switch to standard persister definitions for multi-manager refresh

--- app/models/manageiq/providers/amazon/inventory/persister/definitions/network_collections.rb
+++ app/models/manageiq/providers/amazon/inventory/persister/definitions/network_collections.rb
@@ -13,10 +13,7 @@ module ManageIQ::Providers::Amazon::Inventory::Persister::Definitions::NetworkCo
        load_balancer_listener_pools
        load_balancer_health_checks
        load_balancer_health_check_members).each do |name|
-
-      add_collection(network, name) do |builder|
-        builder.add_properties(:parent => manager.network_manager) if targeted?
-      end
+      add_network_collection(name)
     end
 
     add_cloud_subnet_network_ports
@@ -31,34 +28,30 @@ module ManageIQ::Providers::Amazon::Inventory::Persister::Definitions::NetworkCo
   # ------ IC provider specific definitions -------------------------
 
   def add_network_ports(extra_properties = {})
-    add_collection(network, :network_ports, extra_properties) do |builder|
+    add_network_collection(:network_ports, extra_properties) do |builder|
       if targeted?
         builder.add_properties(:manager_uuids => references(:vms) + references(:network_ports) + references(:load_balancers))
-        builder.add_properties(:parent => manager.network_manager)
       end
     end
   end
 
   def add_floating_ips(extra_properties = {})
-    add_collection(network, :floating_ips, extra_properties) do |builder|
+    add_network_collection(:floating_ips, extra_properties) do |builder|
       if targeted?
         builder.add_properties(:manager_uuids => references(:floating_ips) + references(:load_balancers))
-        builder.add_properties(:parent => manager.network_manager)
       end
     end
   end
 
   def add_cloud_subnet_network_ports(extra_properties = {})
-    add_collection(network, :cloud_subnet_network_ports, extra_properties) do |builder|
+    add_network_collection(:cloud_subnet_network_ports, extra_properties) do |builder|
       builder.add_properties(:manager_ref_allowed_nil => %i(cloud_subnet))
-      builder.add_properties(:parent => manager.network_manager) if targeted?
     end
   end
 
   def add_firewall_rules(extra_properties = {})
-    add_collection(network, :firewall_rules, extra_properties) do |builder|
+    add_network_collection(:firewall_rules, extra_properties) do |builder|
       builder.add_properties(:manager_ref_allowed_nil => %i(source_security_group))
-      builder.add_properties(:parent => manager.network_manager) if targeted?
     end
   end
 end
