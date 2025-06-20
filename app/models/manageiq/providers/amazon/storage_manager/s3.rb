f7cb41c3f79c434f8037b9dfbfb48ed8422e6125
Keenan Brock <keenan@thebrocks.net>
2025-06-20 19:33:26 -0400

    add class_name for virt has_many_through

--- app/models/manageiq/providers/amazon/storage_manager/s3.rb
+++ app/models/manageiq/providers/amazon/storage_manager/s3.rb
@@ -17,7 +17,7 @@ class ManageIQ::Providers::Amazon::StorageManager::S3 < ManageIQ::Providers::Sto
            :allow_nil => true
 
   virtual_has_many :cloud_tenants, :through => :parent_manager
-  virtual_has_many :volume_availability_zones, :through => :parent_manager
+  virtual_has_many :volume_availability_zones, :through => :parent_manager, :class_name => "AvailabilityZone"
 
   def self.ems_type
     @ems_type ||= "s3".freeze
